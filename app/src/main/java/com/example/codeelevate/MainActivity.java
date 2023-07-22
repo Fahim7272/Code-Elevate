732c472d4bfa2be3d40857ba9d03637e09badd59
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-07-23 04:28:55 +0600

    Login Page Completed

--- app/src/main/java/com/example/codeelevate/MainActivity.java
+++ app/src/main/java/com/example/codeelevate/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.codeelevate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button b1, b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.login);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Login",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        b2=(Button)findViewById(R.id.signupp);
+        b2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Sign Up",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
