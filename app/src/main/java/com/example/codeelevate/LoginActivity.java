798d2e42d912d994e2e7c3a279cc18ba1a2b3e2a
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-07-23 15:34:15 +0600

    SignUp Page Completed

--- app/src/main/java/com/example/codeelevate/LoginActivity.java
+++ app/src/main/java/com/example/codeelevate/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.codeelevate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        b1=(Button)findViewById(R.id.login_page_to_signup);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"SignUp",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/codeelevate/SignupActivity.java
+++ app/src/main/java/com/example/codeelevate/SignupActivity.java
@@ -2,13 +2,29 @@ package com.example.codeelevate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
+
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        b1=(Button)findViewById(R.id.signup_page_to_login);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Login",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
