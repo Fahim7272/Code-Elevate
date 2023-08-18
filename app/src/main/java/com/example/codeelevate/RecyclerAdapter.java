ab3be26f4846d152e7a2b1be57d3aa709973ffe2
Ekram <ekram.cse.20210104061@aust.edu>
2023-08-19 01:57:42 +0600

    Recycler View added

---
+++ app/src/main/java/com/example/codeelevate/RecyclerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.codeelevate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.view.menu.MenuView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>
+{
+    String data[];
+    Context context;
+
+    public RecyclerAdapter(Context context,String[] data) {
+        this.data = data;
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view =layoutInflater.inflate(R.layout.custom_design,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerAdapter.ViewHolder holder, int position) {
+
+        holder.button.setText(data[position]);
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        Button button;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            button= itemView.findViewById(R.id.lessson_button);
+        }
+    }
+}
